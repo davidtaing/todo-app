e984abd583ab35ee0ba5dab185468e8b3394349e
David Taing <adavidtaing@gmail.com>
2020-03-08 01:57:01 +1100

    Refactored and cleaned up code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,80 +1,62 @@
 import React, { useState } from 'react';
+
 import AddTaskButton from './components/AddTaskButton';
 import TaskList from './components/TaskList';
 
+const data = [
+  {
+    _id: "0",
+    title: "Do Something",
+    desc: "This is my first task.",
+    complete: false
+  },
+  {
+    _id: "1",
+    title: "Do Another Thing",
+    desc: "This is my second task.",
+    complete: false
+  },
+  {
+    _id: "2",
+    title: "Walk The Dog",
+    desc: "I don't actually have a dog.",
+    complete: false
+  },
+];
+
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      _id: "0",
-      title: "Do Something",
-      desc: "This is my first task.",
-      complete: false
-    },
-    {
-      _id: "1",
-      title: "Do Another Thing",
-      desc: "This is my second task.",
-      complete: false
-    },
-    {
-      _id: "2",
-      title: "Walk The Dog",
-      desc: "I don't actually have a dog.",
-      complete: false
-    },
-  ]);
+  const [tasks, setTasks] = useState(data);
   const [deletedTasks, setDeletedTasks] = useState([]);
   const [taskCounter, setTaskCounter] = useState(3);
 
   const onAddTaskClick = () => {
-    // grab tasks (shallow-copied to avoid mutations)
     const taskList = tasks.slice();
+    const newTask = createTask();
 
-    // create empty data object
-    const data = {
-      _id: taskCounter.toString(),
-      title: "",
-      desc: "",
-      complete: false
-    };
-
+    // TODO remove
     setTaskCounter(taskCounter + 1);
 
-    taskList.push(data);
-
-    // Update State
+    // Push to array and update state
+    taskList.push(newTask);
     setTasks(taskList);
-
-    console.log("Added Task");
   };
 
   const onTaskChecked = (e, data) => {
-    // grab task
     const taskList = tasks.slice();
     const taskIndex = taskList.findIndex((element) => element._id === data._id);
+    const oldTask = taskList[taskIndex];
+
     // update dataset via state change
     if (taskIndex !== -1) {
-      const task = {
-        _id: taskList[taskIndex]._id,
-        title: taskList[taskIndex]._title,
-        desc: taskList[taskIndex].desc,
-        complete: e.target.checked === true
-      }
-
+      const task = createTask(oldTask._id, oldTask.title, oldTask.desc, e.target.checked);
       taskList.splice(taskIndex, 1, task);
       setTasks(taskList);
     }
   }
 
   const onDeleteTaskClick = (taskId) => {
-    console.log(`deleted task ${taskId}`);
-    // Get the task
     let taskList = tasks.slice();
-    console.log(taskList);
-
-    // Find index
     const index = taskList.findIndex((data) => data._id === taskId);
-    console.log(`index ${index}`);
 
     if (index !== -1) {
       // Remove from tasks list
@@ -84,15 +66,26 @@ function App() {
       let deletedTaskList = deletedTasks.slice();
       deletedTaskList.concat(removedTasks);
 
+      // Save state
       setDeletedTasks(deletedTaskList);
       setTasks(taskList);
     }
   }
 
+  const createTask = (_id = taskCounter.toString() , title = "", desc = "", complete=false) => {
+    return {
+      _id,
+      title,
+      desc,
+      complete
+    }
+  }
+
+  // RENDER
   return (
     <div className="App">
       <AddTaskButton onClick={onAddTaskClick} />
-      <TaskList tasks={tasks} onTaskChecked={onTaskChecked} onDeleteTaskClick={onDeleteTaskClick}/>
+      <TaskList tasks={tasks} onTaskChecked={onTaskChecked} onDeleteTaskClick={onDeleteTaskClick} />
     </div>
   );
 }
