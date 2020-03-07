8ab12c6ea90f5e4d1121e23202edcd4ca639742d
David Taing <adavidtaing@gmail.com>
2020-03-07 19:35:36 +1100

    Added skeleton handleAddTaskClick handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import AddTaskButton from './components/AddTaskButton';
 
 function App() {
+  // eslint-disable-next-line
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTaskClick = (tasks) => {
+    console.log("Added Task");
+  };
+
   return (
     <div className="App">
-      <AddTaskButton onClick={() => 0}/>
+      <AddTaskButton onClick={handleAddTaskClick} />
     </div>
   );
 }
