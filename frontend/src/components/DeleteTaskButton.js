19f0bbb88f2b6a05de6a71c712e9cb9bfeacdfad
David Taing <adavidtaing@gmail.com>
2020-03-09 22:42:11 +1100

    Cleaned up DeleteTaskButton

--- frontend/src/components/DeleteTaskButton.js
+++ frontend/src/components/DeleteTaskButton.js
@@ -1,15 +1,13 @@
 import React from "react";
-import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
 const DeleteTaskButton = ({ taskId, onDeleteTaskClick }) => {
     return (
-        <InputGroup.Append>
-            <Button 
-                variant="danger" 
-                onClick={ () => onDeleteTaskClick(taskId) }
-            >Delete</Button>
-        </InputGroup.Append>
+        <Button 
+            variant="danger" 
+            onClick={() => onDeleteTaskClick(taskId)}>
+            Delete
+        </Button>
     );
 }
 
--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 import DeleteTaskButton from './DeleteTaskButton'
 
 const Task = ({ data, onTaskChecked, onDeleteTaskClick }) => {
     const titleText = (data.title || "Add a title for this task.");
+    const descText = (data.desc || "Add a quick summary.")
 
     return (
         <div key={data._id}>
             <Card bg="secondary" text="white" >
                 <Card.Body>
-                    <Card.Title>{titleText}</Card.Title>
-                    <Card.Text>{data.desc}</Card.Text>
-                    { !data.complete ? (<DeleteTaskButton taskId={data._id} onDeleteTaskClick={onDeleteTaskClick}/>) : null}
+                    <Card.Title as={TaskTitle}>{titleText}</Card.Title>
+                    <Card.Text >{descText}</Card.Text>
+                    {<EditTaskButton />}
+                    {!data.complete ? (<DeleteTaskButton taskId={data._id} onDeleteTaskClick={onDeleteTaskClick} />) : null}
                 </Card.Body>
             </Card>
         </div>
+    );
+}
+
+const TaskTitle = (props) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    );
+}
+
+const EditTaskButton = (onClick) => {
+    return (
+        <Button onClick={onClick}>Edit</Button>
     );
 }
 
