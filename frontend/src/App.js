63219bac203603d6dd5d933841747ab85529e90f
David Taing <adavidtaing@gmail.com>
2020-03-09 23:14:55 +1100

    Deleted project files to faciliate a rewrite

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import AddTaskButton from './components/AddTaskButton';
-import TaskList from './components/TaskList';
-
 const data = [
   {
     _id: "0",
@@ -28,61 +25,12 @@ const data = [
 ];
 
 function App() {
+  // eslint-disable-next-line
   const [tasks, setTasks] = useState(data);
+  // eslint-disable-next-line
   const [deletedTasks, setDeletedTasks] = useState([]);
+  // eslint-disable-next-line
   const [taskCounter, setTaskCounter] = useState(3);
-
-  const onAddTaskClick = () => {
-    const taskList = tasks.slice();
-    const newTask = createTask();
-
-    // TODO remove
-    setTaskCounter(taskCounter + 1);
-
-    // Push to array and update state
-    taskList.push(newTask);
-    setTasks(taskList);
-  };
-
-  const onTaskChecked = (e, data) => {
-    const taskList = tasks.slice();
-    const taskIndex = taskList.findIndex((element) => element._id === data._id);
-    const oldTask = taskList[taskIndex];
-
-    // update dataset via state change
-    if (taskIndex !== -1) {
-      const task = createTask(oldTask._id, oldTask.title, oldTask.desc, e.target.checked);
-      taskList.splice(taskIndex, 1, task);
-      setTasks(taskList);
-    }
-  }
-
-  const onDeleteTaskClick = (taskId) => {
-    let taskList = tasks.slice();
-    const index = taskList.findIndex((data) => data._id === taskId);
-
-    if (index !== -1) {
-      // Remove from tasks list
-      const removedTasks = taskList.splice(index, 1);
-
-      // Add to removed tasks list
-      let deletedTaskList = deletedTasks.slice();
-      deletedTaskList.concat(removedTasks);
-
-      // Save state
-      setDeletedTasks(deletedTaskList);
-      setTasks(taskList);
-    }
-  }
-
-  const createTask = (_id = taskCounter.toString(), title = "", desc = "", complete = false) => {
-    return {
-      _id,
-      title,
-      desc,
-      complete
-    }
-  }
 
   // RENDER
   return (
@@ -91,8 +39,7 @@ function App() {
         <Row>
           <Col sm={3}>Menu</Col>
           <Col sm={9} className="main-content">
-            <TaskList tasks={tasks} onTaskChecked={onTaskChecked} onDeleteTaskClick={onDeleteTaskClick} />
-            <AddTaskButton onAddTaskClick={onAddTaskClick} />
+
           </Col>
         </Row>
       </Container>
